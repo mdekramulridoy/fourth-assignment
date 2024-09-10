4136739d22535f6c37d2d933fb1f0370d9c9c97f
ekramulhoqueridoy <158032414+ekramulhoqueridoy@users.noreply.github.com>
2024-09-10 22:29:32 +0600

    push spaces

--- fourth.js
+++ fourth.js
@@ -7,21 +7,28 @@ function calculateTax(income, expenses) {
 // 1. Task for Assignment End
 
 // 2. Task for Assignment Start
+
 function sendNotification(email) {
 
 }
 // 2. Task for Assignment End
+
 // 3. Task for Assignment Start
+
 function checkDigitsInName(name) {
 
 }
 // 3. Task for Assignment End
+
 // 4. Task for Assignment Start
+
 function calculateFinalScore(obj) {
 
 }
 // 4. Task for Assignment End
+
 // 5. Task for Assignment Start
+
 function  waitingTime(waitingTimes  , serialNumber) {
 
 }
